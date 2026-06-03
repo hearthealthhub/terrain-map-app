25a0206149a3db791db7bd093df44fdf8738b28e
hearthealthhub <kingfishbamz@gmail.com>
2026-06-03 18:16:42 +0000

    Deploy GitHub Pages site

---
+++ dist/config.js
@@ -0,0 +1,5 @@
+window.TERRAIN_MAP_CONFIG = {
+  // Optional override example:
+  // imageryTiles: ['https://api.maptiler.com/tiles/satellite-v2/{z}/{x}/{y}.jpg?key=YOUR_KEY'],
+  // labelTiles: ['https://api.maptiler.com/tiles/labels/{z}/{x}/{y}.png?key=YOUR_KEY']
+};
